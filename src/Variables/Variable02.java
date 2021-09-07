82d43014d6204b3d6ba9443b6c1cbb19d6bce183
Umut2020 <69093672+Umut2020@users.noreply.github.com>
2021-09-07 03:41:25 +0200

    completed variable types

---
+++ src/Variables/Variable02.java
@@ -0,0 +1,13 @@
+package Variables;
+
+public class Variable02 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.print("Ali");
+        System.out.println("Can");
+
+        System.out.println("Ali");
+        System.out.println("Can");
+    }
+}
---
+++ src/Variables/Variable03.java
@@ -0,0 +1,35 @@
+package Variables;
+
+public class Variable03 {
+
+    public static void main(String[] args) {
+        int price = 500;
+        System.out.println(price);
+        System.out.println("price");
+
+        int bread = 3;
+        int cheese = 21;
+        System.out.println(bread+cheese);
+
+        boolean old = true;
+        System.out.println(old);
+
+        char firstLetter = 'S';
+        System.out.println(firstLetter);
+
+        byte age = 84;
+        System.out.println(age);
+
+        short villagePopulation = 23000;
+        System.out.println(villagePopulation);
+
+        int treasureMoney = 87463865;
+        System.out.println(treasureMoney);
+
+        float debt = 12.53f; //floatlardan sonra sayıdan sonra 'F' veya 'f' kullanmanız gerekir
+        System.out.println(debt);
+
+        double debt2 = 34.34553465;
+        System.out.println(debt2);
+    }
+}
