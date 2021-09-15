4d6e26869c83defd5798bbb0f0770ad61e23798a
Umut2020 <69093672+Umut2020@users.noreply.github.com>
2021-09-15 23:42:06 +0200

    if else statements

---
+++ src/ifElseStatement/IfElseStatement03.java
@@ -0,0 +1,30 @@
+package ifElseStatement;
+
+import java.util.Scanner;
+
+public class IfElseStatement03 {
+
+    public static void main(String[] args) {
+		/*
+		 *4) Kullanıcıdan bir character alın eğer character bir harf ise ekrana “Harf” yazdırın.
+			Diğer durumlarda ekrana “Harf değil” yazdırın.
+
+		 */
+
+        Scanner scan =new Scanner(System.in);
+        System.out.println("Bir karaekter giriniz");
+        char kar =scan.next().charAt(0);
+
+        if( (kar>='a' && kar<='z') || (kar>='A' && kar<='Z')  ) {
+            System.out.println("Harf");
+        }else {
+            System.out.println("Harf değil");
+        }
+
+        scan.close();
+
+
+
+
+    }
+}
